b5ceb9c6e87917ed80320cd6a754a35a55090902
Jubin Jacob <jubinjacob@Jubins-Mac-mini-2.local>
2014-05-19 09:09:50 +0530

    Initial Commit

---
+++ sampleCalendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sampleCalendar
+//
+//  Created by Jubin Jacob on 19/05/14.
+//  Copyright (c) 2014 Attinad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SampleAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SampleAppDelegate class]));
+    }
+}
